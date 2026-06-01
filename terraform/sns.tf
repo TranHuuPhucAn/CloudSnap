a653a1535714e3d3ea74d117a81791feb946b8c7
TranHuuPhucAn <thpa200903@gmail.com>
2026-06-01 23:53:08 +1000

    Added cloudwatch.tf to monitor metrics and update sns.tf as where to alarm when metrics goes over limit

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -13,3 +13,14 @@ resource "aws_sns_topic_subscription" "email" {
   protocol  = "email"
   endpoint  = var.notification_email
 }
+
+resource "aws_sns_topic" "alarms" {
+  name = "${var.project_name}-alarms"
+  tags = { Project = var.project_name }
+}
+
+resource "aws_sns_topic_subscription" "alarms_email" {
+  topic_arn = aws_sns_topic.alarms.arn
+  protocol  = "email"
+  endpoint  = var.notification_email
+}
